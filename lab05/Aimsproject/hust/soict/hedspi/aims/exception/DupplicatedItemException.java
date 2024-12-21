680c3461e6106a51b83b9944b8ee4c25d9df9532
DangZz07 <vuongvandang14072004@gmail.com>
2024-12-21 21:46:18 +0700

    Final

---
+++ lab05/Aimsproject/hust/soict/hedspi/aims/exception/DupplicatedItemException.java
@@ -0,0 +1,29 @@
+package lab05.Aimsproject.hust.soict.hedspi.aims.exception;
+
+public class DupplicatedItemException extends Exception {
+
+	public DupplicatedItemException() {
+	}
+
+	public DupplicatedItemException(String message) {
+		super(message);
+
+	}
+
+	public DupplicatedItemException(Throwable cause) {
+		super(cause);
+
+	}
+
+	public DupplicatedItemException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public DupplicatedItemException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+
+	}
+
+}
